8ae5f423c0ef92621b7fca2316f703ac33415f6b
kcqua <kweku.quansah@ashesi.edu.gh>
2024-06-01 13:07:43 +0000

    first comit

---
+++ lib-2/global/global_var.dart
@@ -0,0 +1,10 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+String userName = "";
+
+String googleMapKey = "AIzaSyDXHqmJvVJ6gJZs4SmwDt5v6IYizMGGGeM";
+
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
